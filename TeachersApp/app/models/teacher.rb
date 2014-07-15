cc46d8c3c37fa4fbb6870bd6859861b6a7182715
Nelson Taveras <ntaveras03@gmail.com>
2014-07-15 11:34:27 -0400

    Added active_subjects method to teacher model

--- TeachersApp/app/models/teacher.rb
+++ TeachersApp/app/models/teacher.rb
@@ -4,4 +4,8 @@ class Teacher < ActiveRecord::Base
 	accepts_nested_attributes_for :subjects
 	 # reject_if: proc { |attributes| attributes['subject_name'].blank? }
 
+	def active_subjects
+		subjects.select { |s| s.active == true }.length
+	end
+
 end
