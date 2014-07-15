da08c2fcfe6716f13802ca843b32623861a3c484
Nelson Taveras <ntaveras03@gmail.com>
2014-07-15 10:41:08 -0400

    Added teacher model and created index view

--- TeachersApp/config/routes.rb
+++ TeachersApp/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'teachers#index'
+  resources :teachers
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
